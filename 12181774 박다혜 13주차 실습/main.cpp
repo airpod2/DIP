f8087150b3d3f716347c10c6cb2fd0d1d06d62de
dahye <thvldk40@gmail.com>
2021-06-09 17:37:13 +0900

    make panorama image

---
+++ 12181774 박다혜 13주차 실습/main.cpp
@@ -0,0 +1,13 @@
+//#include "panorama.h"
+//
+//int main() {
+//	//<stithcer사용>
+//	//ex_panorama_simple(); //2장은 성공! 3장은 오른쪽 사진이 안됨
+//	
+//	//<단계별 구현>
+//	//ex_panorama(); //성공!
+//
+//	BooknScene();
+//
+//	return 0;
+//}
